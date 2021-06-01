b1f7c9a3013fb5c5008e2aec7461da6fcb641bbd
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2021-05-31 20:08:39 -0400

    primer upload

---
+++ database/factories/PersonaFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Persona;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PersonaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Persona::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre'=> $this->faker->name(),
+            'apellidoPaterno'=> $this->faker->firstName(),
+            'apellidoMaterno'=> $this->faker->lastName(),
+            'ci'=> $this->faker->randomNumber(),
+            'sexo'=> $this->faker->name()
+        ];
+    }
+}
